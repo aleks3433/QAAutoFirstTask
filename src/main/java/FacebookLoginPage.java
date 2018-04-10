42bffd7f2f89eb4447995f1b945be0511e924c97
MATLASHEVSKYI OLEKSANDR <aleks3433@mail.ru>
2018-04-10 22:50:07 +0300

    Update FacebookLoginPage.java

--- src/main/java/FacebookLoginPage.java
+++ src/main/java/FacebookLoginPage.java
@@ -5,15 +5,17 @@ public class FacebookLoginPage {
     private static final String LOGIN = "";
     private static final String PASSWORD = "";
 
-    public void inputLogin(){
+    public static FacebookLoginPage inputLogin(){
         BrowserManager.browser.findElement(By.xpath("//input[@id='email']")).sendKeys(LOGIN);
+        return new FacebookLoginPage();
     }
 
-    public void inputPassword(){
+    public static FacebookLoginPage inputPassword(){
         BrowserManager.browser.findElement(By.xpath("//input[@id='pass']")).sendKeys(PASSWORD);
+        return new FacebookLoginPage();
     }
 
-    public FacebookProfilePage clickLoginButton(){
+    public static FacebookProfilePage clickLoginButton(){
         BrowserManager.browser.findElement(By.cssSelector(".uiButton.uiButtonConfirm")).click();
         return new FacebookProfilePage();
     }
