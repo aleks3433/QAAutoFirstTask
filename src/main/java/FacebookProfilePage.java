e3d39e9c73b52d50645d17d12265c1b9d1f69b89
MATLASHEVSKYI OLEKSANDR <aleks3433@mail.ru>
2018-04-10 22:51:23 +0300

    Update FacebookProfilePage.java

--- src/main/java/FacebookProfilePage.java
+++ src/main/java/FacebookProfilePage.java
@@ -4,8 +4,9 @@ import org.testng.Assert;
 
 public class FacebookProfilePage {
 
-    public void findProfileIcon() {
+    public static void findProfileIcon() {
         WebElement findProfileIcon = BrowserManager.browser.findElement(By.cssSelector("._2qgu._7ql._1m6h.img"));
         Assert.assertTrue(findProfileIcon.isDisplayed());
     }
 }
+
