95ed5bf3c8ce066897994c032e4622edb900124d
aleks3433 <aleks3433@mail.ru>
2018-04-20 23:22:49 +0300

    Page Object and Page Factory with static

--- src/main/java/FacebookLoginPage.java
+++ src/main/java/FacebookLoginPage.java
@@ -2,18 +2,20 @@ import org.openqa.selenium.By;
 
 public class FacebookLoginPage {
 
-    private static final String LOGIN = "";
-    private static final String PASSWORD = "";
+    private static final String LOGIN = "aleks3433@mail.ru";
+    private static final String PASSWORD = "0938942957нфдгидгвкгяуш";
 
-    public void inputLogin(){
+    public static FacebookLoginPage inputLogin(){
         BrowserManager.browser.findElement(By.xpath("//input[@id='email']")).sendKeys(LOGIN);
+        return new FacebookLoginPage();
     }
 
-    public void inputPassword(){
+    public static FacebookLoginPage inputPassword(){
         BrowserManager.browser.findElement(By.xpath("//input[@id='pass']")).sendKeys(PASSWORD);
+        return new FacebookLoginPage();
     }
 
-    public FacebookProfilePage clickLoginButton(){
+    public static FacebookProfilePage clickLoginButton(){
         BrowserManager.browser.findElement(By.cssSelector(".uiButton.uiButtonConfirm")).click();
         return new FacebookProfilePage();
     }
--- src/main/java/FacebookProfilePage.java
+++ src/main/java/FacebookProfilePage.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 
 public class FacebookProfilePage {
 
-    public void findProfileIcon() {
+    public static void findProfileIcon() {
         WebElement findProfileIcon = BrowserManager.browser.findElement(By.cssSelector("._2qgu._7ql._1m6h.img"));
         Assert.assertTrue(findProfileIcon.isDisplayed());
     }
